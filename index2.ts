9128a4233644238459ca7b0b2b8c32240a6ac5b3
NutSrimueng <156981371+NutSrimueng@users.noreply.github.com>
2024-08-30 10:45:13 +0700

    Create index2.ts

---
+++ index2.ts
@@ -0,0 +1,22 @@
+// const animals : string ='หมา*แมว*กระต่าย*ม้า'
+// const animalsArray : string[] = animals.split('*')
+// console.log('เดิม',animalsArray)
+// animalsArray.pop()
+// console.log('ดึงตัวท้าย',animalsArray)
+
+// animalsArray.push('งู')
+// console.log('เพิ่มเข้ามา',animalsArray)
+
+// animalsArray.shift()
+// console.log('เอาตัวหน้าออก',animalsArray)
+
+// animalsArray.unshift('เสือ')
+// console.log('ใส่ตัวหน้า',animalsArray)
+// console.log('เรียงก-ฮ',animalsArray.sort())
+// console.log('เรียงฮ-ก',animalsArray.reverse())
+// console.log('slice 0-2',animalsArray.slice(0,2))
+// const fruits : string = 'Banana,Orange,Apple,Mango'
+// const fruitsArray : string[] = fruits.split(',')
+// console.log(fruitsArray)
+// console.log('เรียงA-Z',fruitsArray.sort())
+// console.log('เรียงZ-A',fruitsArray.reverse())
